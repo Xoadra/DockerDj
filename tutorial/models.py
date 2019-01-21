5996def94b9ee60b3fde78706979b8290a65d4a4
Xoadra <xijp77@gmail.com>
2019-01-21 02:50:30 -0800

    hooked up mysql with new word model

--- tutorial/models.py
+++ tutorial/models.py
@@ -5,3 +5,17 @@
 from django.db import models
 
 
+
+class Word( models.Model ):
+	word = models.CharField( max_length = 50, blank = True )
+	created_at = models.DateTimeField( auto_now_add = True )
+	modified_at = models.DateTimeField( auto_now = True )
+	
+	def __str__( self ):
+		return self.word
+	
+	
+	class Meta:
+		db_table = 'words'
+
+
--- tutorial/views.py
+++ tutorial/views.py
@@ -5,11 +5,15 @@
 import socket
 from django.shortcuts import render
 
+from tutorial.models import Word
+
 
 
 def index( request ):
+	words = Word.objects.all( )
+	empty = Word( word = '<em>no words available</em>' )
 	context = { 'name': 'zoey', 'host': socket.gethostname( ) }
+	context[ 'words' ] = words if len( words ) else [ empty ]
 	return render( request, 'tutorial/index.html', context )
-
 
 
